9a41e0b824e5ed068669d47358c446f2d3885839
joao victor benedet machado <jvbm2177@gmail.com>
2025-02-15 02:34:09 -0300

    first commit

---
+++ src/main/java/com/example/demo/model/Seller.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "O nome do vendedor não pode ser vazio")
+    @Size(min = 2, max = 100, message = "O nome do vendedor deve ter entre 2 e 100 caracteres")
+    private String name;
+
+    @NotEmpty(message = "O email nao deve ser vazio")
+    @Size(min = 8, max = 100, message = "O email deve ter entre 8 e 100 caracteres")
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    private double baseSalary;
+
+    public Seller() {}
+
+    @ManyToOne
+    @JoinColumn(name = "departmentId")  // Chave estrangeira para o Department
+    private Department department;
+
+    public Seller(Integer id, String name, Date birthDate, double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+}
